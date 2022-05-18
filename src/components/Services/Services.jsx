c9139b6c032e89e380864a5b87942e8accebdb1b
Gavryk <gvozdyk23@gmail.com>
2022-05-18 21:55:48 +0300

    components So far, no style

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import style from './Services.module.scss';
+//icons
+import clock from '../../img/clock.svg';
+import diaphragm from "../../img/diaphragm.svg";
+import money from "../../img/money.svg";
+import teamwork from "../../img/teamwork.svg";
 
 const Services = () => {
   return (
     <div className={style.servicesSection}>
-
+      <div className={style.description}>
+        <h2>
+          Hight <span>quality</span> services
+        </h2>
+        <div className={style.cards}>
+          <div className={style.card}>
+            <div className={style.icon}>
+              <img src={clock} alt="Clock" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+          <div className={style.card}>
+            <div className={style.icon}>
+              <img src={teamwork} alt="Clock" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+          <div className={style.card}>
+            <div className={style.icon}>
+              <img src={diaphragm} alt="Clock" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+          <div className={style.card}>
+            <div className={style.icon}>
+              <img src={money} alt="Clock" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Services
